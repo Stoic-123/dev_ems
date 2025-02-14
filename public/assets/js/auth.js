3c2ec6837e19aa4034a384243befa9bc9ff2d454
Stoic-123 <longbigboss123@gmail.com>
2025-02-14 15:42:06 +0700

    l

--- public/assets/js/auth.js
+++ public/assets/js/auth.js
@@ -27,7 +27,7 @@ document.getElementById('loginbtn').addEventListener('click', async function (ev
     const password = passwordInput.value.trim();
 
     try {
-        const response = await fetch('http://localhost:3000/api/login', {
+        const response = await fetch('https://copebeta.site/api/login', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
