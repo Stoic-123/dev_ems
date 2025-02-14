e75e0cffe3cbcfbc0dfa8da3a574ba32fef280e2
Stoic-123 <longbigboss123@gmail.com>
2025-02-14 16:18:09 +0700

    k

--- public/assets/js/auth.js
+++ public/assets/js/auth.js
@@ -27,7 +27,7 @@ document.getElementById('loginbtn').addEventListener('click', async function (ev
     const password = passwordInput.value.trim();
 
     try {
-        const response = await fetch('https://qr1.copebeta.publicvm.com/api/login', {
+        const response = await fetch('/api/login', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
